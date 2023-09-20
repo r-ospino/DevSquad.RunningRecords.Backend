12d7e946c9de84b8915b2417a07fb2b0e4215220
Rafael Ospino <rospino@gmail.com>
2023-09-20 08:55:14 -0500

    Implements Record retrieval

---
+++ src/DevSquad.RunningRecords.Backend.Api/RunningRecordsEndpoints/GetARunEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using DevSquad.RunningRecords.Backend.Application.RunningRecordUseCases;
+using FastEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DevSquad.RunningRecords.Backend.Api.RunningRecordsEndpoints;
+
+[HttpGet("/api/run/{id}")]
+[AllowAnonymous]
+public class GetARunEndpoint : Endpoint<GetARunCommand, RecordDto?>
+{
+    private readonly IMediator _mediator;
+
+    public GetARunEndpoint(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async override Task HandleAsync(GetARunCommand req, CancellationToken ct)
+    {
+        var record = await _mediator.Send(req, ct);
+        await SendAsync(record, cancellation: ct);
+    }
+}
---
+++ src/DevSquad.RunningRecords.Backend.Application/RunningRecordUseCases/GetARunHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DevSquad.RunningRecords.Backend.Domain;
+using MediatR;
+using Throw;
+
+namespace DevSquad.RunningRecords.Backend.Application.RunningRecordUseCases
+{
+    public class GetARunHandler : IRequestHandler<GetARunCommand, RecordDto?>
+    {
+        private readonly IRunningRecordRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetARunHandler(IRunningRecordRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<RecordDto?> Handle(GetARunCommand request, CancellationToken cancellationToken)
+        {
+            Guid.TryParse(request.Id, out var recordId).Throw().IfFalse();
+            Record? record = await _repository.GetByIdAsync(recordId, cancellationToken);
+            return _mapper.Map<RecordDto>(record);
+        }
+    }
+}
