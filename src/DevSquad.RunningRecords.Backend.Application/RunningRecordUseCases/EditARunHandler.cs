4f828a0c020423536063569d0f9e3542735bac54
Rafael Ospino <rospino@gmail.com>
2023-09-20 01:28:20 -0500

    Implements a Record edition

---
+++ src/DevSquad.RunningRecords.Backend.Application/RunningRecordUseCases/EditARunHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using DevSquad.RunningRecords.Backend.Domain;
+using MediatR;
+using Throw;
+
+namespace DevSquad.RunningRecords.Backend.Application.RunningRecordUseCases;
+
+public class EditARunHandler : IRequestHandler<EditARunCommand, RecordDto>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRunningRecordRepository _repository;
+    private readonly IMapper _mapper;
+
+    public EditARunHandler(IUnitOfWork unitOfWork, IRunningRecordRepository repository, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<RecordDto> Handle(EditARunCommand request, CancellationToken cancellationToken)
+    {
+        Guid.TryParse(request.Id, out var recordId).Throw().IfFalse();
+        DateTime startDate = request.StartDate.ThrowIfNull();
+        DateTime endDate = request.EndDate.ThrowIfNull().IfLessThanOrEqualTo(startDate);
+        Enum.TryParse<DistanceUnits>(request.DistanceUnit, true, out var unit);
+
+        Record record = (await _repository.GetByIdAsync(recordId, cancellationToken)).ThrowIfNull();
+        record.Date = startDate;
+        record.Duration = endDate.Subtract(startDate);
+        record.Distance = new(request.DistanceMagnitude.ThrowIfNull(), unit);
+        record.Steps = request.Steps.ThrowIfNull();
+
+        await _unitOfWork.CommitAsync();
+
+        return _mapper.Map<RecordDto>(record);
+    }
+}
