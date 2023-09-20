329b7acfa60523f82f7f44a714124426c8c88d9b
Rafael Ospino <rospino@gmail.com>
2023-09-20 00:11:33 -0500

    Fixes and completes unit tests for Record entity

--- tests/DevSquad.RunningRecords.Backend.Domain.Tests/RecordTests.cs
+++ tests/DevSquad.RunningRecords.Backend.Domain.Tests/RecordTests.cs
@@ -8,10 +8,13 @@ namespace DevSquad.RunningRecords.Backend.Domain.Tests;
 public class RecordTests
 {
     [Theory, InlineAutoData]
-    public void Constructor_ShouldSetPropertiesCorrectly(DateTime date, int minutes, Distance distance, int steps)
+    public void Constructor_ShouldSetPropertiesCorrectly(int minutes, Distance distance, int steps)
     {
-        // Act
+        // Arrange
+        var date = DateTime.Now;
         var duration = TimeSpan.FromMinutes(minutes);
+
+        // Act
         var record = new Record(date, duration, distance, steps);
 
         // Assert
@@ -26,13 +29,42 @@ public class RecordTests
     [Theory]
     [InlineAutoData(0)]
     [InlineAutoData(-1)]
-    public void Constructor_ShouldThrowForInvalidDuration(int minutes, DateTime date, Distance distance, int steps)
+    public void Constructor_ShouldThrowForNegativeOrZeroDuration(int minutes, Distance distance, int steps)
     {
         // Arrange
+        var date = DateTime.Now;
         var duration = TimeSpan.FromMinutes(minutes);
 
         // Act
         var act = () => new Record(date, duration, distance, steps);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory, InlineAutoData]
+    public void Constructor_ShouldThrowForFutureDate(int days, int minutes, Distance distance, int steps)
+    {
+        // Arrange
+        var date = DateTime.Now.AddDays(days);
+        var duration = TimeSpan.FromMinutes(minutes);
+
+        // Act
+        var act = () => new Record(date, duration, distance, steps);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory, InlineAutoData]
+    public void Steps_ShouldThrowForNegativeSteps(int minutes, Distance distance, int steps)
+    {
+        // Arrange
+        var date = DateTime.Now;
+        var duration = TimeSpan.FromMinutes(minutes);
+
+        // Act
+        var act = () => new Record(date, duration, distance, -steps);
 
         // Assert
         act.Should().Throw<ArgumentException>();
